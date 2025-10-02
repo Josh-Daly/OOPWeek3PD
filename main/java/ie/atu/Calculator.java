32451b4edb86be9c2891d4b254264d367c48ca3a
Paul_Daly <g00470372@atu.ie>
2025-10-02 14:42:34 +0100

    Added new method to subtract numbers in calculator

--- main/java/ie/atu/Calculator.java
+++ main/java/ie/atu/Calculator.java
@@ -1,13 +1,13 @@
 package ie.atu;
 
 public class Calculator {
-    public int add(int firNum, int secNum) { //Function to add two numbers
+    public int add(int firNum, int secNum) { //Function to add two numbers *
         return firNum + secNum;
     }
 
-    //public int subtract(int firNum, int secNum) { //Function to subtract two numbers
-    //  return firNum - secNum;
-    // }
+    public int sub(int firNum, int secNum) { //Function to subtract two numbers *
+      return firNum - secNum;
+    }
 
 
 }
