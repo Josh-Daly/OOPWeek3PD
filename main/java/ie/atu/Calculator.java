1858a186219e637675521b04116ab9b7a4f5429f
Paul_Daly <g00470372@atu.ie>
2025-10-02 14:55:30 +0100

    Added new method to divide numbers in calculator, and fix bug with second number allowing 0 as an input

--- main/java/ie/atu/Calculator.java
+++ main/java/ie/atu/Calculator.java
@@ -1,15 +1,20 @@
 package ie.atu;
 
 public class Calculator {
-    public int add(int firNum, int secNum) { //Function to add two numbers *
+    public int add(int firNum, int secNum) { //Method to add two numbers *
         return firNum + secNum;
     }
 
-    public int sub(int firNum, int secNum) { //Function to subtract two numbers *
+    public int sub(int firNum, int secNum) { //Method to subtract two numbers *
       return firNum - secNum;
     }
 
-    public int mul(int firNum, int secNum) { //Function to multiply two numbers *
+    public int mul(int firNum, int secNum) { //Method to multiply two numbers *
         return firNum * secNum;
     }
+
+    public int div(int firNum, int secNum) { //Method to divide two numbers
+        return firNum / secNum;
+    }
+
 }
