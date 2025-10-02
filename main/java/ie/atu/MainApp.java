307968923ce95ab993338f70bae0d056b440000f
Paul_Daly <g00470372@atu.ie>
2025-10-02 14:36:50 +0100

    Added new method to add numbers in calculator

--- main/java/ie/atu/MainApp.java
+++ main/java/ie/atu/MainApp.java
@@ -4,5 +4,27 @@
 
 package ie.atu;
 
+import java.util.Scanner;
+
 public class MainApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Calculator calc1 = new Calculator(); //Creates a calculator object
+
+        System.out.println("Simple Calculator - Add Two Numbers");
+        System.out.print("Enter first number: ");
+        int a = scan.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scan.nextInt();
+
+        int result = calc1.add(a,b);
+        System.out.println("Result of operation: " + result);
+
+        scan.close();
+    }
+
+
+
+
 }
